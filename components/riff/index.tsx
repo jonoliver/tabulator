7dfb78b31bcf1fc842e9bf7924af4fc545c46b8b
Jon Oliver <jon@heysparkbox.com>
2022-10-02 01:27:09 -0400

    lil better

--- components/riff/index.tsx
+++ components/riff/index.tsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { mergeDeepRight } from 'ramda';
+import { MouseEventHandler, useState } from "react";
+import produce from "immer";
 import styles from './riff.module.css';
 
 type ID = string;
@@ -47,33 +47,69 @@ const mockState: Riff = {
     }
   ]
 }
+
+type ShowProps = {
+  when: boolean,
+  children: JSX.Element,
+}
+const Show = ({ when, children }: ShowProps) => when ? children : null;
+
+type NoteProps = {
+  note: Fret,
+  setNote: MouseEventHandler
+}
+
+const Note = ({ note, setNote }: NoteProps) => {
+  const hasNote = note.number !== undefined;
+  const noteClassNames = [styles.fret, !hasNote && styles.empty].filter(Boolean).join(' ');
+  return (
+    <div
+      className={noteClassNames}
+      onClick={setNote}
+    >
+      <Show when={hasNote}>
+        <span className={styles.number}>{note.number}</span>
+      </Show>
+    </div>
+  )
+}
+
 const Riff = () => {
   const [riff, setRiff] = useState(mockState);
+  const [pasteValue, setPasteValue] = useState(0);
   const setNote = (strungId: ID, noteId: ID) => {
-    const strung = riff.strungs.find(x => x.id === strungId);
-    const note = strung?.notes.find(x => x.id === noteId);
-    console.log(riff.strungs.find(x => x.id === strungId)?.notes.find(x => x.id === noteId))
+    if (isNaN(pasteValue)) return;
+
+    setRiff(produce((draft) => {
+      const note = draft.strungs
+        .find(x => x.id === strungId)?.notes
+        .find(x => x.id === noteId);
+      if (note) {
+        note.number = note.number === pasteValue ? undefined : pasteValue;
+      }
+    }));
   }
+
   return <div className={styles.riff}>
     {
       riff.strungs.map((strung) =>
         <div key={strung.id} className={styles.string}>
           {
-            strung.notes.map((note) => note.number
-              ?
-              <div key={note.id} className={styles.fret}>
-                <span className={styles.number}>{note.number}</span>
-              </div>
-              :
-              <div
-                key={note.id}
-                className={`${styles.fret} ${styles.empty}`}
-                onClick={() => setNote(strung.id, note.id)}
-              />
+            strung.notes.map(note =>
+              <Note key={note.id} note={note} setNote={() => setNote(strung.id, note.id)} />
             )
           }
-        </div>)
+        </div>
+      )
     }
+    <input
+      type="number"
+      min="0"
+      max="25"
+      style={{ width: '5rem', padding: '0.5rem', paddingRight: '0', margin: 'auto' }}
+      value={pasteValue}
+      onChange={(e) => setPasteValue(parseInt(e.target.value, 10))}
+    />
   </div>
 }
 
