4cc71787516570f4efc2779af5092acf2e452fd4
Jon Oliver <jon@heysparkbox.com>
2022-10-10 18:21:52 -0400

    fix babel for tests

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,10 @@
 module.exports = {
-  presets: [
-    ['@babel/preset-env', { targets: { node: 'current' } }],
-    '@babel/preset-typescript',
-  ],
+  env: {
+    test: {
+      presets: [
+        ['@babel/preset-env', { targets: { node: 'current' } }],
+        '@babel/preset-typescript',
+      ],
+    }
+  }
 };
