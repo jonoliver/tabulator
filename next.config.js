ba2c98819e49c95ea0a9a546466522d34f1fa790
Jon Oliver <jon@heysparkbox.com>
2023-02-23 00:02:51 -0500

    ci: add ASSET_PREFIX env var

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
+const { ASSET_PREFIX } = process.env;
 const nextConfig = {
+  assetPrefix: ASSET_PREFIX,
   reactStrictMode: true,
   swcMinify: true,
   experimental: {
